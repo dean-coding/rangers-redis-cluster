68977ce1c65a93a412469b7283b4cd4a816b688b
fuhw/vencano <mac@macdeMacBook-Pro-4.local>
2018-04-06 19:01:20 +0800

    add:redis 单点的配置

--- src/main/java/com/rangers/redis/cluster/config/RedisClusterConfig.java
+++ src/main/java/com/rangers/redis/cluster/config/RedisClusterConfig.java
@@ -2,7 +2,11 @@ package com.rangers.redis.cluster.config;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
@@ -15,14 +19,36 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisClusterConfig {
 
+	private static final Logger log = LoggerFactory.getLogger(RedisClusterConfig.class);
+
 	@Autowired
-	RedisClusterConfigProperties clusterProperties;
+	private RedisClusterConfigProperties clusterProperties;
+	@Autowired
+	private RedisProperties redisProperties;
+	@Value("${redis.mode.cluster:false}")
+	private Boolean redisModeCluster;
 
 	public @Bean RedisConnectionFactory connectionFactory() {
-
-		List<String> nodes = clusterProperties.getNodes();
-		System.out.println("配置的节点信息:" + nodes);
-		return new JedisConnectionFactory(new RedisClusterConfiguration(nodes));
+		log.info("当前redis的连接模式是否是集群:{}", redisModeCluster);
+		JedisConnectionFactory factory;
+		if (redisModeCluster == null || !redisModeCluster) {
+			factory = new JedisConnectionFactory();
+			String host = redisProperties.getHost();
+			String password = redisProperties.getPassword();
+			int database = redisProperties.getDatabase();
+			int port = redisProperties.getPort();
+			factory.setHostName(host);
+			factory.setPassword(password);
+			factory.setDatabase(database);
+			factory.setPort(port);
+			factory.setTimeout(redisProperties.getTimeout());
+			log.info("配置的连接factory配置信息:host={},password={},database={},port={}", host, password, database, port);
+		} else {
+			List<String> nodes = clusterProperties.getNodes();
+			factory = new JedisConnectionFactory(new RedisClusterConfiguration(nodes));
+			log.info("配置的连接factory配置信息:{}", nodes);
+		}
+		return factory;
 	}
 
 	public @Bean RedisTemplate<String, Object> getRedisTemplate(RedisConnectionFactory factory) {
